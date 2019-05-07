a7d58a0214be594a6b9cf888b0e02dbce254f81c
dswanson8 <d.swan0204@gmail.com>
2019-05-07 08:38:19 -0600

    added new projects

---
+++ Profile Page/profile_page_ver1.1/js/main.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//     $("#js-block").hover(function(){
+//         console.log('hello world');
+//       $(this).animate({
+//           padding: '20',
+//           background: 'red'
+//       });
+//     });
+//   });
